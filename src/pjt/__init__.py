cc2e62c4de526f58e07a55a06d30c50ef43e1251
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2022-11-20 15:16:25 +0000

    build: add `poetry`

---
+++ src/pjt/__init__.py
@@ -0,0 +1,13 @@
+"""pjt - a quick navigation tool for the PyPI packages."""
+
+__title__ = "pjt"
+__summary__ = "pjt - a quick navigation tool for the PyPI packages."
+__uri__ = "https://github.com/volopivoshenko/pjt"
+
+__version__ = "0.0.1"
+
+__author__ = "Volodymyr Pivoshenko"
+__email__ = "volodymyr.pivoshenko@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022, Volodymyr Pivoshenko"
