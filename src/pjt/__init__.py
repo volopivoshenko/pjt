6b7ee27ce28113e68748d43037ff3b1b1fd4a0df
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2022-11-20 20:28:03 +0000

    chore: bump version

--- src/pjt/__init__.py
+++ src/pjt/__init__.py
@@ -4,7 +4,7 @@ __title__ = "pjt"
 __summary__ = "pjt - a quick navigation tool for the PyPI packages."
 __uri__ = "https://github.com/volopivoshenko/pjt"
 
-__version__ = "0.0.1"
+__version__ = "0.0.0"
 
 __author__ = "Volodymyr Pivoshenko"
 __email__ = "volodymyr.pivoshenko@gmail.com"
